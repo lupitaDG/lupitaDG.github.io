ec9be581530daae79864e2d372617901edc30ce5
lupitaDG <117939640+lupitaDG@users.noreply.github.com>
2022-12-14 13:54:20 -0600

    Add files via upload

---
+++ juego.js
@@ -0,0 +1,147 @@
+
+var a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,
+b10,c6,c11, d6,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,
+f6,g6,h6,i3,i4,i5,i6,i7,i8,i9,j6;
+
+//palabra JavaScript color rojo
+function comprobar1(){
+a1 = document.getElementById('celda1').style.color="red";
+}
+
+function comprobar2(){
+a2 = document.getElementById('celda2').style.color="red";
+}
+function comprobar3(){
+a3 = document.getElementById('celda3').style.color="red";
+}
+function comprobar4(){
+a4 = document.getElementById('celda4').style.color="red";
+}
+
+function comprobar5(){
+a5 = document.getElementById('celda5').style.color="red";
+}
+
+function comprobar6(){
+a6 = document.getElementById('celda6').style.color="red";
+}
+
+function comprobar7(){
+a7 = document.getElementById('celda7').style.color="red";
+}
+function comprobar8(){
+a8 = document.getElementById('celda8').style.color="red";
+}
+function comprobar9(){
+a9 = document.getElementById('celda9').style.color="red";
+}
+function comprobar10(){
+a10 = document.getElementById('celda10').style.color="red";
+}
+//palabra estrella
+function comprobarB8(){
+b8 = document.getElementById('celdaB8').style.color="blue";
+}
+function comprobarC7(){
+c7 = document.getElementById('celdaC7').style.color="blue";
+}
+function comprobarD6(){
+  d6 = document.getElementById('celdaD6').style.color="blue";
+}
+function comprobarE5(){
+  e5 = document.getElementById('celdaE5').style.color="blue";
+}
+function comprobarF4(){
+  f4 = document.getElementById('celdaF4').style.color="blue";
+}
+function comprobarG3(){
+  g3 = document.getElementById('celdaG3').style.color="blue";
+}
+function comprobarH2(){
+  h2 = document.getElementById('celdaH2').style.color="blue";
+}
+function comprobarI1(){
+  i1 = document.getElementById('celdaI1').style.color="blue";
+}
+//palabra nebulosa color cafe
+function comprobarB2(){
+b2 = document.getElementById('celdaB2').style.color="brown";
+}
+function comprobarC2(){
+c2 = document.getElementById('celdaC2').style.color="brown";
+}
+function comprobarD2(){
+d2 = document.getElementById('celdaD2').style.color="brown";
+}
+function comprobarE2(){
+e2 = document.getElementById('celdaE2').style.color="brown";
+}
+function comprobarF2(){
+f2 = document.getElementById('celdaF2').style.color="brown";
+}
+function comprobarG2(){
+g2 = document.getElementById('celdaG2').style.color="brown";
+}
+function comprobarH2(){
+h2 = document.getElementById('celdaH2').style.color="brown";
+}
+function comprobarI2(){
+i2 = document.getElementById('celdaI2').style.color="brown";
+}
+//palabra galaxia
+function comprobarD4(){
+d4 = document.getElementById('celdaD4').style.color="yellow";
+}
+function comprobarD5(){
+d5 = document.getElementById('celdaD5').style.color="yellow";
+}
+function comprobarD6(){
+d6 = document.getElementById('celdaD6').style.color="yellow";
+}
+function comprobarD7(){
+d7 = document.getElementById('celdaD7').style.color="yellow";
+}
+function comprobarD8(){
+d8 = document.getElementById('celdaD8').style.color="yellow";
+}
+function comprobarD9(){
+  d9 = document.getElementById('celdaD9').style.color="yellow";
+}
+function comprobarD10(){
+  d10 = document.getElementById('celdaD10').style.color="yellow";
+}
+//palabra lactea color orange
+function comprobarI4(){
+i4 = document.getElementById('celdaI4').style.color="orange";
+}
+function comprobarI5(){
+i5 = document.getElementById('celdaI5').style.color="orange";
+}
+function comprobarI6(){
+  i6 = document.getElementById('celdaI6').style.color="orange";
+}
+function comprobarI7(){
+i7 = document.getElementById('celdaI7').style.color="orange";
+}
+function comprobarI8(){
+i8 = document.getElementById('celdaI8').style.color="orange";
+}
+function comprobarI9(){
+i9 = document.getElementById('celdaI9').style.color="orange";
+}
+//palabra polvo color verde
+function comprobarG6(){
+g6 = document.getElementById('celdaG6').style.color="green";
+}
+  function comprobarG7(){
+g7 = document.getElementById('celdaG7').style.color="green";
+}
+  function comprobarG8(){
+g8 = document.getElementById('celdaG8').style.color="green";
+}
+  function comprobarG9(){
+g9 = document.getElementById('celdaG9').style.color="green";
+}
+  function comprobarG10(){
+g10 = document.getElementById('celdaG10').style.color="green";
+}
